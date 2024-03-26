c4f763feab79026aee8635ae1730f09fbbb3ece6
Zeetars <zeetars@gmail.com>
2024-03-26 12:55:47 +0200

    Add project files.

---
+++ FruitSA.Model/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitSA.Model
+{
+    internal class Category
+    {
+        [Required]
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string CategoryCode { get; set; }
+        [Required]
+        public byte IsActive { get; set; } = 1; 
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ FruitSA.Model/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FruitSA.Model
+{
+    public class Product
+    {
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductCode { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        [ForeignKey("nameof(CategoryKey)")]
+        public int CategoryId { get; set; }       
+        [Required]
+        public decimal Price { get; set; }
+        public string? Image { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
