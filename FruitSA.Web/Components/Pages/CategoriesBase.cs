35cb7415c651733981c33a96ac2cff3baf2dbe17
Zivanai Taruvinga <zivanaitaruvinga@yahoo.co.uk>
2024-03-28 20:33:12 +0200

    Categories Done, Can View, Add and Update, now Starting on Excel Uploads

--- FruitSA.Web/Components/Pages/CategoriesBase.cs
+++ FruitSA.Web/Components/Pages/CategoriesBase.cs
@@ -19,6 +19,9 @@ namespace FruitSA.Web.Components.Pages
         public Category Category { get; set; } = new Category();
         public IEnumerable<Category> Categories { get; set; } = Enumerable.Empty<Category>();
 
+        public UniqueCodeValidator UniqueCodeValidator { get; set; }
+
+        public string? errorMessage;
         protected override async Task OnInitializedAsync()
         {
             Categories = (await CategoryService.GetCategories()).ToList();
@@ -31,5 +34,57 @@ namespace FruitSA.Web.Components.Pages
             }          
 
         }
+
+        protected async Task HandleValidSubmit()
+        {
+            int.TryParse(Id, out int CategoryId);
+            
+            Category result = null;
+            if (CategoryId != 0)
+            {
+
+                result = await CategoryService.UpdateCategory(Category);
+
+            }
+            else
+            {
+                errorMessage = "";
+                var uniqueCode = Category.CategoryCode.ToUpper();
+                if (uniqueCode != null)
+                {
+                    bool isValide;
+                    var notAvailable = Categories.FirstOrDefault(c => c.CategoryCode == uniqueCode);
+                    if (notAvailable == null)
+                    {
+                        isValide = UniqueCodeValidator.Validate(uniqueCode);
+                        if (!isValide)
+                        {
+                            errorMessage = "Category Code is required in the format ABC123.";
+                            return;
+                        }
+
+                        Category.CategoryCode = uniqueCode;
+                    }
+                    else 
+                    {
+                        errorMessage = "Category Code already assigned to another Category.";
+                        return;
+                    }
+                }
+                else
+                {
+                    errorMessage = "Category Code is required in the format ABC123.";
+                    return;
+                }
+
+                result = await CategoryService.CreateCategory(Category);
+            }
+
+            if (result != null)
+            {
+                NavigationManager.NavigateTo("/categories");
+            }
+        }
+       
     }
 }
---
+++ FruitSA.Web/Providers/UniqueCodeValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FruitSA.Web.Providers
+{
+    public class UniqueCodeValidator
+    {
+
+        public static bool Validate(string code)
+        {
+            bool isValid = CheckFormat(code);
+            return isValid;
+        }
+
+        public static bool CheckFormat(string code)
+        {
+            // Regular expression pattern to match 3 alphabetic letters followed by 3 numeric characters
+            string pattern = @"^[A-Za-z]{3}\d{3}$";
+
+            // Check if input matches the pattern
+            return Regex.IsMatch(code, pattern);
+        }
+    }
+
+
+}
